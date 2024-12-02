d51ca646f871deed2ed790ad0a9581ac1c301ab3
FizzyElt <fizzyelt.dev@outlook.com>
2024-12-02 14:43:00 +0800

    day2 q1

--- day2/q1.ml
+++ day2/q1.ml
@@ -1,1 +1,37 @@
-let () = ()
+open Lib
+
+type sort =
+  | Non
+  | Increasing
+  | Decreasing
+
+let parse_line line = line |> String.split_on_char ' ' |> List.map int_of_string
+let increasing_or_decreasing a b = if a > b then Increasing else Decreasing
+
+let is_in_range a b =
+  let diff = a - b in
+  abs diff <= 3 && abs diff > 0
+;;
+
+let is_safe list =
+  let rec loop prev list =
+    match prev, list with
+    | _, [ _ ] | _, [] -> true
+    | Non, a :: b :: tl ->
+      if is_in_range a b
+      then (
+        let t = increasing_or_decreasing a b in
+        loop t (b :: tl))
+      else false
+    | Increasing, a :: b :: tl ->
+      let t = increasing_or_decreasing a b in
+      if t = Increasing && is_in_range a b then loop t (b :: tl) else false
+    | Decreasing, a :: b :: tl ->
+      let t = increasing_or_decreasing a b in
+      if t = Decreasing && is_in_range a b then loop t (b :: tl) else false
+  in
+  loop Non list
+;;
+
+let result list = list |> List.filter is_safe |> List.length
+let () = Sys.argv.(1) |> File.read_list_of_line parse_line |> result |> print_int
