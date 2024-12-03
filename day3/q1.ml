4f8fcc7a9e449f1175cd110112cd4548bf4c80d0
FizzyElt <fizzyelt.dev@outlook.com>
2024-12-03 21:49:41 +0800

    day3 q1

--- day3/q1.ml
+++ day3/q1.ml
@@ -1,1 +1,30 @@
-let () = ()
+open Lib
+
+let rec find_all_matches reg str pos =
+  try
+    let result = Str.search_forward reg str pos in
+    let matched = Str.matched_string str in
+    matched :: find_all_matches reg str (result + 1)
+  with
+  | Not_found -> []
+;;
+
+let mul_regexp = Str.regexp {|mul([0-9]+,[0-9]+)|}
+
+let mul_two_nums str =
+  let reg = Str.regexp {|\([0-9]+\)|} in
+  let rest = Str.substitute_first reg (fun _ -> "") str in
+  let l = Str.matched_string str |> int_of_string in
+  let _ = Str.substitute_first reg (fun _ -> "") rest in
+  let r = Str.matched_string rest |> int_of_string in
+  l * r
+;;
+
+let () =
+  Sys.argv.(1)
+  |> File.read_file
+  |> fun str ->
+  find_all_matches mul_regexp str 0
+  |> List.fold_left (fun acc str -> mul_two_nums str + acc) 0
+  |> print_int
+;;
